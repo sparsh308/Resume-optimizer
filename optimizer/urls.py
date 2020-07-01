b1886f62561120c745a24bce4c82ce9f3f6dd9f3
Sparsh kishore kumar <sparsh308@gmail.com>
2020-07-01 13:34:52 +0530

    new commit

--- optimizer/urls.py
+++ optimizer/urls.py
@@ -24,6 +24,3 @@ urlpatterns = [
 ]
 urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
-urlpatterns = [
-    # ... the rest of your URLconf goes here ...
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
